424ef2ae8a7af6afb8255cf144b6c128c71a1293
CarbonLollipop <georgiy.rha@gmail.com>
2023-06-22 09:32:05 -0700

    Customizable values and removed borders

--- main.c
+++ main.c
@@ -1,3 +1,16 @@
+// You can configure the amount of food by passing an argument to the program
+// Configure everything else here
+
+// The color of the snake
+#define SNAKE_COLOR COLOR_GREEN
+#define SNAKE_BACKGROUND_COLOR COLOR_BLACK
+
+// For example, this value set to 2 would make the snake two times faster
+#define GAME_SPEED_MULTIPLIER 1
+
+// How much score you get for eating food
+#define FOOD_SCORE 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
@@ -5,11 +18,9 @@
 #include <unistd.h>
 #include <time.h>
 
-void quit(int score) {
-    erase();
+void quit() {
+    endwin(); 
     curs_set(1);
-    endwin();
-    printf("Game over! Score: %d\n", score);
     exit(0);
 }
 
@@ -43,8 +54,8 @@ int main(int argc, char** argv) {
     start_color();
     keypad(stdscr, TRUE);
 
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    init_pair(2, COLOR_RED, COLOR_BLACK);
+    init_pair(1, SNAKE_COLOR, SNAKE_BACKGROUND_COLOR);
+    init_pair(2, COLOR_RED, SNAKE_BACKGROUND_COLOR);
 
     noecho();
     curs_set(0);
@@ -52,40 +63,45 @@ int main(int argc, char** argv) {
     int x, y;
     getmaxyx(stdscr, y, x);
 
-    char* horizontalWall = malloc(x + 1);
+    //char* horizontalWall = malloc(x + 1);
 
-    for (int i = 0; i < x; i++)
-        horizontalWall[i] = '~';
+    //for (int i = 0; i < x + 1; i++)
+    //    horizontalWall[i] = '~';
 
-    horizontalWall[x] = '\0';
-    printw("%s", horizontalWall);
+    //horizontalWall[x] = '\0';
+    //printw("%s", horizontalWall);
 
-    for (int i = 0; i < y - 2; i++) {
-        move(i + 1, 0);
-        printw("|");
-        move(i + 1, x - 1);
-        printw("|");
-    }
+    //for (int i = 0; i < y - 1; i++) {
+    //    move(i + 1, 0);
+    //    printw("|");
+    //    move(i + 1, x - 1);
+    //    printw("|");
+    //}
 
-    move(y - 1, 0);
-    printw("%s", horizontalWall);
+    //move(y - 1, 0);
+    //printw("%s", horizontalWall);
 
     int snakePos[2] = {y / 2, x / 2};
     // MEGA HAX
     int score = 2;
     int snakeBody[2048][2];
 
+    // note to future self: atoi means ascii to integer
+    // its a stupid name
+
     int food = (argc > 1) ? atoi(argv[1]) : 20;
 
     for (int i = 0; i < food; i++) {
         drawFood(x, y);
     }
+    
+    int lost = 0;
 
     while(1) {
         int c = getch();
 
         if (c == 'q')
-            break;
+            quit();
         if ((c == 'h' || c == KEY_LEFT) && direction != 3)
            direction = 0;
         if ((c == 'j' || c == KEY_DOWN) && direction != 2)
@@ -106,19 +122,19 @@ int main(int argc, char** argv) {
 
         for (int i = 0; i < score; i++) {
             if (snakePos[0] == snakeBody[i][0] && snakePos[1] == snakeBody[i][1])
-                quit(score - 2); 
+                lost = 1;
         }
 
         if(mvinch(snakePos[0], snakePos[1]) == '@') {
-            score++;
+            score += FOOD_SCORE;
             drawFood(x, y);
         }
 
         if (snakePos[0] == 0 || snakePos[0] == y - 1 || snakePos[1] == 0 || snakePos[1] == x - 1)
-            break;
+            lost = 1;
 
         for (int i = 0; i < score; i++) {
-            move(snakeBody[i][0], snakeBody[i][1]);
+            if(mvinch(snakeBody[i][0], snakeBody[i][1]) != '|') 
             printw(" ");
         }
 
@@ -127,28 +143,38 @@ int main(int argc, char** argv) {
             snakeBody[i][1] = snakeBody[i - 1][1];
         }
 
-        attron(COLOR_PAIR(1));
+        lost ? attron(COLOR_PAIR(2)) : attron(COLOR_PAIR(1));
 
         for (int i = 0; i < score; i++) {
             move(snakeBody[i][0], snakeBody[i][1]);
             printw("o");
         }
 
-        attroff(COLOR_PAIR(1));
-
         snakeBody[0][0] = snakePos[0];
         snakeBody[0][1] = snakePos[1];
 
         move(snakePos[0], snakePos[1]);
-        attron(COLOR_PAIR(1));   
+
         printw("#");
-        attroff(COLOR_PAIR(1));
+       
+        lost ? attroff(COLOR_PAIR(2)) : attroff(COLOR_PAIR(1));
 
         refresh();
-        
-        usleep(70000 - (score * 1000));
-    }
+      
+        if(lost)
+            break;
 
+        usleep(((50000 - (score * 1000) > 20000 ? 50000 - (score * 1000) : 20000) * 2) / GAME_SPEED_MULTIPLIER);
+    }
+    
+    move(y / 2, (x / 2) - 10);
+    printw("Game over! Score: %d", score);
+    move((y / 2) + 1, (x / 2) - 10);
+    printw("Hit any key to exit...");
+    curs_set(1);
+    nodelay(stdscr, FALSE);
+    getch();
+    
     quit(score - 2);
 }
 
